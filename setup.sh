fb1a324201c3f869594ee4192793f7f862791ecb
joonas <joonas8cohen@gmail.com>
2023-10-03 20:53:28 -0700

    script updates

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 sudo apt upgrade && sudo apt update
 sudo apt install git openssh-server
-git clone https://github.com/joonascohen/dot.files.github
-cd dot.files
 xargs -a pkglist.txt sudo apt install
