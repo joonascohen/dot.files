f3b893bbcdaf789a8aba9b61282907dd0604296f
joonas <joonas8cohen@gmail.com>
2023-10-03 21:26:20 -0700

    changes

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ xargs -a pkglist.txt sudo apt install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cd
+rm -rf .zshrc
+cd dot.files
+stow zsh nvim
+source ~/.zshrc
